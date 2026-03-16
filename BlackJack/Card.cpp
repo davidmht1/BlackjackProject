85451c34c3f160e3d5a9e925f69e4f68f2c1a0c7
tho7293-droid <tho7293@sdsu.edu>
2026-03-15 19:48:27 -0700

    Update Card class.

--- BlackJack/Card.cpp
+++ BlackJack/Card.cpp
@@ -1,46 +1,34 @@
 #include "Card.h"
 
-Card::Card(RANK rank = ACE, SUIT suit = SPADES, bool isUp = true)
+Card::Card(RANK rank, SUIT suit, bool isUp)
 {
 	this->rank = rank;
 	this->suit = suit;
 	this->isFaceUp = isUp;
 }
 
+// Return the value of cards
 int Card::getValue()
 {
-	if (isFaceUp)
-	{
-		switch (suit) 
-		{
-		case ACE:
-			return 11;
-		case TWO:
-			return 2;
-		case THREE:
-			return 3;
-		case FOUR:
-			return 4;
-		case FIVE:
-			return 5;
-		case SIX:
-			return 3;
-		case SEVEN:
-			return 4;
-		case EIGHT:
-			return 5;
-		case NINE:
-			return 9;
-		default:
-			return 10;
-		}
-	}
-	return 0;
-}
+	if (!isFaceUp)
+		return 0;
 
+	switch (rank)
+	{
+		case ACE: return 11;
+		case TWO: return 2;
+		case THREE: return 3;
+		case FOUR: return 4;
+		case FIVE: return 5;
+		case SIX: return 6;
+		case SEVEN: return 7;
+		case EIGHT: return 8;
+		case NINE: return 9;
+		default: return 10;
+	}
+}
+//Flips the card state.
 void Card::flip()
 {
-	if (!isFaceUp)
-		isFaceUp = true;
-	// else print card is already up?
+	isFaceUp = !isFaceUp;
 }
